b6e20c7fe07fe203f1a5e7a648dd682866a442d9
houlad <athoul01@gmail.com>
2025-08-01 13:05:56 -0400

    -Initial commit. -These are mainly exploratory garbage atm

---
+++ helper_functions.R
@@ -0,0 +1,39 @@
+filter_by_condition <- function(df, condition, ...){
+  #filters the data based on supplied conditions and then collects, returning a tibble
+  df |> 
+    filter({{condition}}, ...) |> 
+    collect()
+  
+}
+
+select_by_variable <- function(df, variable, ...){
+  df |> 
+    select({{variable}}) |> 
+    collect()
+}
+
+add_dates <- function(df){
+  df |> 
+    mutate(
+      dow = wday(CheckoutDateTime, label = TRUE),
+      month = month(CheckoutDateTime),
+      hour = hour(CheckoutDateTime),
+      week = week(CheckoutDateTime)
+    )
+}
+
+add_age_group <- function(df){
+  df |> 
+    mutate(age = case_when(
+      Collection %in% (type_data |> filter(age_group == 'Teen') |> pull(code)) ~ "Teen",
+      Collection %in% (type_data |> filter(age_group == 'Juvenile') |> pull(code)) ~ "Juvenile",
+      Collection %in% (type_data |> filter(age_group == 'Adult') |> pull(code)) ~ "Adult"),
+    )
+}
+
+
+get_year_and_add_age_group <- function(df, condition){
+  df |> 
+    filter_by_condition({{condition}}) |> 
+    add_age_group()
+}
